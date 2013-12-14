ad84646eaa414861d898e957811d46825b5e6c9c
JCPedroza <nemesis4go10@hotmail.com>
2013-12-14 04:15:55 -0700

    changed imports formatting

--- com/smash/Transfertron5000/Checksum.java
+++ com/smash/Transfertron5000/Checksum.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
 import org.apache.commons.codec.digest.DigestUtils;
 
 // Class that contains methods that handle checksum generation.
